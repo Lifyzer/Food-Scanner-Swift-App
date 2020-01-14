0e4f01b151814b86b8e7c41f1cc6078ca4683250
Pierre-Henry Soria <hi@ph7.me>
2020-01-14 19:13:08 +1300

    Update git remote path

--- save-project.sh
+++ save-project.sh
@@ -9,7 +9,7 @@ function save-project-to-repo() {
 declare readonly gitRemotes=(
     git@bitbucket.org:pH_7/food-scanner-swift-app.git
     git@gitlab.com:pH-7/food-scanner-swift-app.git
-    git@github.com:pH-7/Food-Scanner-Swift-App.git
+    git@github.com:Lifyzer/Food-Scanner-Swift-App.git
 )
 for remote in "${gitRemotes[@]}"
 do
